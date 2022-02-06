3003f8c9963632d62ce9642f68abfc273cffc1ba
i028ff <kousuke111125@gmail.com>
2022-02-06 21:49:42 +0900

    aaaa

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -6,15 +6,18 @@ async function getData() {
     { id: 1, first_name: '優', family_name: '大木', affilication: 'TechTrain', is_student: false },
     { id: 2, first_name: '太郎', family_name: '山田', affilication: 'HogeHoge大学', is_student: true }
   ];
-  const result = await test()
+  const result = await test(userList)
   return await result
 }
 
-function test() {
+function test(data) {
   return new Promise(() => {
     setTimeout(() => {
-      const full_name = userList.map((userList) => userList["full_name"] = userList.family_name + " " + userList.first_name)
-      return userList;
+      const userList2 = data.map(() => {
+        data["full_name"] = data.family_name + " " + data.first_name
+        return data;
+      })
+      return userList2;
     }, 3000)
   });
 }
