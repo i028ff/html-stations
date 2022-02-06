f8641877dd33e2c2d99a90597cf20fd5d73e2a45
i028ff <kousuke111125@gmail.com>
2022-02-06 12:33:48 +0900

    aaaa

--- src/scripts/station14.js
+++ src/scripts/station14.js
@@ -1,4 +1,3 @@
-const { fromNullabla } = ("fp-ts/lib/Option")
 
 function getData() {
     // これから作ろうとしているもので、JavaScriptでフルネームを生成する必要が出てきました。
@@ -8,15 +7,15 @@ function getData() {
         { id: 1, first_name: '優', family_name: '大木', affilication: 'TechTrain', is_student: false },
         { id: 2, first_name: '太郎', family_name: '山田', affilication: 'HogeHoge大学', is_student: true }
     ];
-    return test.map(buildFullName)
+    return test.map(buildFullName(test))
 }
 
 function buildFullName(data) {
     // Station14の問題はこちらです。想定する答えをここを書いてください。
     // 氏名がわかるようにしつつ、半角スペースで繋いでください。
     // またtest配列のそれぞれのオブジェクトに対して、full_nameのプロパティが追加されるように実装すること
-    const fullname = test.family_name + " " + test.first_name
-    data["full_name"] = fullname;
+    const full_name = data.family_name + " " + data.first_name
+    data[1]=[full_name, full_name]
     return data
 }
 
